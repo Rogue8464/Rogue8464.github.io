cdf7ce65d48944b8d2327903a8d5f7d6cdeee4dc
Rogue8464 <133009983+Rogue8464@users.noreply.github.com>
2023-09-18 21:53:45 +0800

    Add files via upload

--- 計算.js
+++ 計算.js
@@ -1,10 +1,11 @@
 var budget = 0; //預算變數
 var spent = 0; //支出變數
-var month = 5;
-var year = 2023;
-const date = [];
+var currentdate = new Date();
+var month = currentdate.getMonth() + 1;
+currentdate.setDate(1);
+//const date = [];
 var days;
-var space = 1;
+var space = currentdate.getDay();
 if(month==1  || month==3 || month==5 || month==7 || month==8 || month==10 || month==12){
     days = 31;
 }
@@ -17,6 +18,7 @@ else if(month == 2){
 else{
     days = 30;
 }
+
 for(var i=0;i<space;i++){
     cal.innerHTML = cal.innerHTML + `
         <button id="useless" style="width:80px;margin:5px 5px 5px 5px;visibility:hidden;">useless</button>
@@ -39,8 +41,8 @@ function count(){
 }
 count();
 reset.addEventListener("click",function(){
-    budget = parseInt(prompt("請輸入預算" , 0));
-    spent = parseInt(prompt("請輸入支出" , 0));
+    budget = parseInt(prompt("請輸入預算"));
+    spent = parseInt(prompt("請輸入支出"));
     count();
 })
 addbudget.addEventListener("click",function(){
@@ -59,8 +61,8 @@ function date_click(x){
 }
 sub.addEventListener("click",function(){
     const regex = /^[0-9]+$/;
-    if(!regex.test(document.getElementById("money").value)){
-        alert("請輸入數字");
+    if(!regex.test(document.getElementById("money").value) || document.getElementById("money").value <= 0){
+        alert("請輸入有效數字");
         document.getElementById("money").value="0";
         document.getElementById("reason").value="";
     }
@@ -73,8 +75,13 @@ sub.addEventListener("click",function(){
             line+=" 收入 ";
             budget = budget + parseInt(document.getElementById("money").value);
         }
-        line = line + parseInt(document.getElementById("money").value) + " 元\n原因: ";
-        line+=document.getElementById("reason").value;
+        line = line + parseInt(document.getElementById("money").value) + " 元";
+        record.innerHTML = record.innerHTML + `
+            <p>${line}</p>
+            <p>原因:${document.getElementById("reason").value}</p>
+            <hr/>
+        `;
+        line = line + "\n原因: " + document.getElementById("reason").value;
         document.getElementById("input").style.display = "none";
         document.getElementById("img").style.display = "none";
         document.getElementById("money").value="0";
